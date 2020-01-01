e88b74691fc17ee8b619b08b1bfb00d2618f4984
root <root@vultr.guest>
2020-01-01 15:27:51 +0000

    unwrap_or_else

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}",config.query);
-    println!("In file {}",config.filename);
+    println!("Searching for {}",&config.query);
+    println!("In file {}",&config.filename);
 
-    let contents = fs::read_to_string(config.filename)
+    let contents = fs::read_to_string(&config.filename)
         .expect("Something went wrong reading the file");
 
-    println!("With text:\n{}", contents);
+    println!("With text:\n{}", &contents);
 
 }
 
@@ -21,9 +25,16 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+                return Err("not enough arguments") 
+        }
 
-    Config{ query, filename }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        
+        Ok(Config{ query, filename })
+    }
 }
